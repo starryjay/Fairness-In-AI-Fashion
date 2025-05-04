4f3180e62e7bb031c66cf2cb94248adc9de0798f
starryjay <sshree@usc.edu>
2025-05-04 01:30:38 -0700

    Updated code

--- analysis.py
+++ analysis.py
@@ -9,7 +9,6 @@ def load_data(data_file_path):
     data = pd.read_csv(data_file_path)
     return data
 
-#Ho: We hypothesize that gender and skin tone has no impact on the number of runways, magazine covers, and awards that models earn. 
 def import_skintone_data():
     skintones_first75 = pd.read_csv('data/skintones_first75.csv')
     skintones_last75 = pd.read_csv('data/skintones_last75.csv')
@@ -18,9 +17,7 @@ def import_skintone_data():
     skintones = skintones.drop_duplicates(subset=['name'], keep='first')
     if 'Unnamed: 0' in skintones.columns:
         skintones = skintones.drop(columns=['Unnamed: 0'])
-    # split RGB values into separate columns
     skintones[['R', 'G', 'B']] = skintones['RGB_value'].str.split(',', expand=True).astype(int)
-    # drop the original RGB_value column
     skintones = skintones.drop(columns=['RGB_value'])
     skintones = skintones.reset_index(drop=True)
     skintones.to_csv('data/skintones.csv', index = False)
@@ -29,11 +26,8 @@ def import_skintone_data():
 
 def skintone_vs_runway(skintones_df, moty_df, top_50_f, top_50_m, agency_df):
     combined_data = pd.merge(moty_df, skintones_df, on='name', how='inner').rename(columns={'skin_tone_y': 'skin_tone', 'skin_tone_x': 'skin_tone_moty', 'current_agency_affiliation':'current_agency'})
-    #print('moty:', combined_data.columns)
     combined_data_f = pd.merge(top_50_f, skintones_df, on='name', how='inner').rename(columns={'skin_tone_y': 'skin_tone', 'skin_tone_x': 'skin_tone_top_50_f', 'hair ': 'hair_color', 'eyes ': 'eye_color'})
-    #print('top_50_f:', combined_data_f.columns)
     combined_data_m = pd.merge(top_50_m, skintones_df, on='name', how='inner').rename(columns={'skin_tone_y': 'skin_tone', 'skin_tone_x': 'skin_tone_top_50_m', 'hair ': 'hair_color', 'eyes ': 'eye_color'})
-    #print('top_50_m:', combined_data_m.columns)
     combined_data = pd.concat([combined_data, combined_data_f, combined_data_m], axis=0).drop(columns=['agency_city', 'agency_country'])
     agency_df = agency_df.drop(columns=['female', 'male', 'non_binary', 'city', 'country']).rename(columns={'agency_name': 'current_agency'})
     combined_data = pd.merge(combined_data, agency_df, on='current_agency', how='left')
@@ -54,7 +48,7 @@ def analysis_runway_shows(combined_data):
     anova_table_shows = sm.stats.anova_lm(model_runway_shows, typ=2) 
     anova_table_shows = anova_table_shows.fillna(0)
     print("ANOVA Results using statsmodels [Runway Shows]:\n", anova_table_shows)
-    residuals = model_runway_shows.resid  # Extract residuals from ANOVA model
+    residuals = model_runway_shows.resid
     plt.figure(figsize=(8, 5))
     sns.histplot(residuals, bins=20, kde=True)
     plt.title("Residuals Distribution Runway Shows")
@@ -73,7 +67,7 @@ def analysis_magazine_covers(combined_data):
      anova_table_covers = sm.stats.anova_lm(model_magazine_covers, typ=2) 
      anova_table_covers = anova_table_covers.fillna(0)
      print("ANOVA Results using statsmodels [ Magazine Covers]:\n", anova_table_covers)
-     residuals = model_magazine_covers.resid  # Extract residuals from ANOVA model
+     residuals = model_magazine_covers.resid
      plt.figure(figsize=(8, 5))
      sns.histplot(residuals, bins=20, kde=True)
      plt.title("Residuals Distribution Covers")
@@ -94,7 +88,7 @@ def anallysis_awards(combined_data):
     anova_table_award = sm.stats.anova_lm(model_awards, typ=2) 
     anova_table_award = anova_table_award.fillna(0)
     print("ANOVA Results using statsmodels [Awards]:\n", anova_table_award)
-    residuals = model_awards.resid  # Extract residuals from ANOVA model
+    residuals = model_awards.resid
     plt.figure(figsize=(8, 5))
     sns.histplot(residuals, bins=20, kde=True)
     plt.title("Residuals Distribution Awards")
@@ -165,7 +159,6 @@ def main():
     top_50_female = load_data('data/Top_50_F.csv')
     top_50_male = load_data('data/Top_50_M.csv')
 
-    #remove white space
     moty_df['name'] = moty_df['name'].str.strip()
     moty_df['award'] = moty_df['award'].str.strip()
     moty_df['hair_color'] = moty_df['hair_color'].str.lower().str.strip()
@@ -173,15 +166,8 @@ def main():
     moty_df['choice'] = moty_df['choice'].str.strip()
     moty_df['gender'] = moty_df['gender'].str.strip()   
 
-    #remove duplicate names 
-    #moty_df = moty_df.drop_duplicates(subset=['name'], keep='first')
-
-    #preprocess
-
-    #Model of the Year
     moty_df_model_of_the_year = moty_df[moty_df['award'] == 'MOTY']
 
-    #break out star
     moty_bs_df = moty_df[moty_df['award'] == 'BS']
 
     combined_data = skintone_vs_runway(skintones_df, moty_df_model_of_the_year, top_50_female, top_50_male, agency_df)
@@ -194,14 +180,5 @@ def main():
     plot_interaction(combined_data, 'num_achievements')
 
     regresseion_analysis(combined_data)
-
-    
-    
-  
-
-
-    #TODO: top 50 - female
-
-    #TODO: top 50 - male'''
 
 main()
